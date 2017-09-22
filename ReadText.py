2b38530c367abed7fe29ea14823495107da6a5c7
sonimore <smsoniamoreno@gmail.com>
2017-09-22 19:23:42 +0200

    Scraping data from web

--- ReadText.py
+++ ReadText.py
@@ -1,29 +1,29 @@
-''' Sonia Moreno, 9/20/2017
+'''Sonia Moreno, 9/20/2017
  Reads and parses a text file containing course schedule information.
  '''
+
+from bs4 import BeautifulSoup
+import requests
+html = requests.get('https://apps.carleton.edu/campus/registrar/schedule/enroll/?term=18WI&subject=CS').text
+soup = BeautifulSoup(html, 'html5lib')
 def main():
-	f = open('SampleText', 'r')
-	courses = f.readlines()
-	for line in courses:
-		print(line)
-		if "CS" in line:
-			tempCourse = line
-		if "Lecture/Discussion" in line:
-			tempTime = line
-			break
+	# print(html)
+	a_schedule = {'1a': '8:30', '2a': '9:50', '3a': '11:10', '4a': '12:30', '5a':'1:50', '6a': '3:10'}
+
+	# first_course = soup.find('h3')
+	# first_course_text = soup.div.text
+	# fi = soup.div['id']
+	first_course = soup('div', "course")
+	print(first_course)
+	print(len(first_course))
+	# print(first_course_text)
 
 
 
-
-	data = {
-    "Course Number":tempCourse,
-    # "Course Title":courses[2],
-    "Time":tempTime
-	}
-	print(data)
 
 
 if __name__ == '__main__':
 	main()
+
 
 
