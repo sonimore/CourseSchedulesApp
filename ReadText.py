42751cde0c00d4bc51f1185bfdfec1cb486926d2
sonimore <smsoniamoreno@gmail.com>
2017-10-20 23:01:45 +0200

    Code for edge cases, fixed bugs, now creates one unified list/csv file to be used in API development

--- ReadText.py
+++ ReadText.py
@@ -54,7 +54,7 @@ def Subject():
 	for item in subjects:
 		subj_list.append(item.get_text())
 
-	print subj_list
+	# print subj_list
 
 	# Each item in subj_list is currently in the form: 'Computer Science (CS)'
 	# We only want the abbrevation in the parentheses so that we can use this in the html link
@@ -68,68 +68,91 @@ def Subject():
 	return subj_abbrev
 
 
+
 ''' Returns dict object with course number, course name, and start/end times for each course
 Finds course info based on the academic term and subject chosen (in this case, Winter 2018)
 '''
-def Specific_Course_Info():
-	# Course listings for computer science during winter term of 2018
-	html = requests.get('https://apps.carleton.edu/campus/registrar/schedule/enroll/?term=18WI&subject=CS').text
-	soup = BeautifulSoup(html, 'html5lib')
 
-	# Creates list of all items with course as class attribute
-	course_summary = soup.find_all("div", class_="course")
-
+def Specific_Course_Info(term):
 	# Creates dict object with course number as key and list containing name and times for course as values
 	course_info = defaultdict(list)
-	for course in course_summary:
-		course_num = course.find(class_= "coursenum").get_text()
 
-		# Finds title attribute within each course
-		title = course.find(class_ = "title").get_text()
+	subjects = Subject()
 
-		# Only takes the actual name of the course
-		# which is next to the coursenum attribute but not within its own tag
-		for item in title:
-			course_name = course.find(class_= "coursenum").next_sibling	
+	for subject in subjects:
+		html_string = 'https://apps.carleton.edu/campus/registrar/schedule/enroll/?term=' + term + '&subject=' + subject
 
-		# Start and end times for course
-		start_time = course.find(class_ = "start").get_text()
-		end_time = course.find(class_ = "end").get_text()
+		# Course listings for subject during term provided
+		html = requests.get(html_string).text
 
-		# Add info to list associated with key
-		course_info[course_num].append(course_name)
-		course_info[course_num].append(start_time)
-		course_info[course_num].append(end_time)
-	
+		soup = BeautifulSoup(html, 'html5lib')
+
+		# Creates list of all items with course as class attribute, excluding related courses
+		course_summary = soup.find_all("div", class_="course")
+		for course in course_summary:
+			course_num = course.find(class_= "coursenum").get_text()
+			# Finds title attribute within each course
+			title = course.find(class_ = "title").get_text()
+			# Only takes the actual name of the course
+			# which is next to the coursenum attribute but not within its own tag
+			for item in title:
+				course_name = course.find(class_= "coursenum").next_sibling	
+
+			# Add info to list associated with key
+			if course_num.find(subject) > -1:	
+				course_info[course_num].append(course_name)	
+				# Start and end times for courses that have set times
+				# Account for classes without set times
+				if course.find(class_ = "start") != None:
+					start_time = course.find("span", {"class": "start"}).get_text()
+					end_time = course.find(class_ = "end").get_text()
+					course_info[course_num].append(start_time)
+
+					course_info[course_num].append(end_time)
+	print course_info
 	# Creates csv file with course info
-	with open('course_info.csv', 'w') as f:
+	with open('course_info7.csv', 'w') as f: 
 		w = csv.DictWriter(f, course_info.keys())
 		w.writeheader()
 		w.writerow(course_info)
+
 	return course_info
+''' Adds lists together from Specific_Course_Info so that each csv file will contain info 
+for ALL subjects in one term
+''' 
+def Append_Dicts(a_dict, b_dict):
+	return
 
 
-''' Returns HTML string that Specific Course Info will use to provide only the information
-from the academic term and subject chosen.
+''' Returns HTML string that Specific Course Info will use to provide information
+for every term and subject combination.
 '''
 def Generate_HTML():
-	terms = Academic_Term()
-	subjects = Subject()
+	terms = Academic_Term()[1:3]
+	# print "terms:"
+	# print terms
+	subjects = Subject()[1:3]
+	# print subjects
 	html = []
 	for term in terms:
 		for subject in subjects:
-			html_string = 'https://apps.carleton.edu/campus/registrar/schedule/enroll/?term=' + term + '&subject=' + subject
-			html.append(html_string)
+			print term
+			print subject
+		Specific_Course_Info(term, subject)
+			# html_string = 'https://apps.carleton.edu/campus/registrar/schedule/enroll/?term=' + term + '&subject=' + subject
+			# Specific_Course_Info(html_string)
+			# html.append(html_string)
+	# print html_string
+	# print html
+	
 
-	print html
-	return html
 
 def main():
 	
-	Academic_Term()
-	Subject()
-	Generate_HTML()
-	# Specific_Course_Info()
+	# Academic_Term()
+	# Subject()
+	# Generate_HTML()
+	Specific_Course_Info('18WI')
 
 main()
 
